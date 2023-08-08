7fb2d7009df46eb0b1ae4e4cf383bc074539a07b
Luxamar Lourvens <63268101+Lourvens@users.noreply.github.com>
2023-08-08 18:14:33 +0200

    update skill section;

---
+++ src/sections/skills/iconItems.jsx
@@ -0,0 +1,52 @@
+import typescriptImg from "../../assets/tech/typeScript.png";
+import PostgresSQLImg from "../../assets/tech/PostgresSQL.png";
+import NestjsImg from "../../assets/tech/Nestjs.png";
+import ReactImg from "../../assets/tech/React.png";
+import GitImg from "../../assets/tech/Git.png";
+import MongoImg from "../../assets/tech/MongoDB.png";
+import NodeImg from "../../assets/tech/Node.png";
+import JavaScriptImg from "../../assets/tech/JavaScript.png";
+import JestImg from "../../assets/tech/Jest.png";
+// import PostgresSQLImg from "../../assets/tech/PostgresSQL.png";
+// import PostgresSQLImg from "../../assets/tech/PostgresSQL.png";
+
+const items = [
+  {
+    name: "TypeScript",
+    imgSrc: typescriptImg,
+  },
+  {
+    name: "Nodejs",
+    imgSrc: NodeImg,
+  },
+  {
+    name: "JavaScript",
+    imgSrc: JavaScriptImg,
+  },
+  {
+    name: "React",
+    imgSrc: ReactImg,
+  },
+  {
+    name: "Jest",
+    imgSrc: JestImg,
+  },
+  {
+    name: "Git",
+    imgSrc: GitImg,
+  },
+  {
+    name: "PostgresSQL",
+    imgSrc: PostgresSQLImg,
+  },
+  {
+    name: "Nestjs",
+    imgSrc: NestjsImg,
+  },
+  {
+    name: "MongoDb",
+    imgSrc: MongoImg,
+  },
+];
+
+export default items;
--- src/sections/skills/iconRow.jsx
+++ src/sections/skills/iconRow.jsx
@@ -1,16 +1,16 @@
 import React from "react";
-import skillIcons from "../../data/techIcons";
+import items from "./iconItems";
 
 export default function IconRow() {
   return (
     <div className="flex flex-wrap justify-center gap-4 ">
-      {skillIcons.map(({ name, Icon }) => (
+      {items.map(({ name, imgSrc }) => (
         <div
           key={name}
           className="w-16 h-16 p-4 bg-white rounded-full dark:bg-white dark:bg-opacity-10 cursor-pointer"
           title={name}
         >
-          <Icon />
+          <img src={imgSrc} alt={`${name} logo`} />
         </div>
       ))}
     </div>
